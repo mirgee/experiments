0bbbc5e43c2c93b64d3ea6cb8606d0f223f11e42
Miroslav Kovar <miroslav.kovar@absa.africa>
2023-04-14 16:24:16 +0200

    did_resolver_sov to write a test
    Signed-off-by: Miroslav Kovar <miroslav.kovar@absa.africa>

--- did_resolver_sov/src/lib.rs
+++ did_resolver_sov/src/lib.rs
@@ -49,7 +49,65 @@ impl DIDResolvable for DIDSovResolver {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use aries_vcx_core::indy::ledger::pool::open_pool_ledger;
+    use aries_vcx_core::indy::ledger::pool::{
+        create_pool_ledger_config, open_pool_ledger,
+        test_utils::{create_tmp_genesis_txn_file, delete_test_pool, open_test_pool},
+    };
+
+    struct SetupProfile {
+        pub institution_did: String,
+        pub profile: Arc<dyn Profile>,
+        pub(self) teardown: Arc<dyn Fn() -> BoxFuture<'static, ()>>,
+    }
+
+    impl SetupProfile {
+        pub async fn init() -> SetupProfile {
+            init_test_logging();
+            set_test_configs();
+            SetupProfile::init_indy().await
+        }
+
+        async fn init_indy() -> SetupProfile {
+            let (institution_did, wallet_handle) = setup_issuer_wallet().await;
+
+            settings::set_config_value(
+                settings::CONFIG_GENESIS_PATH,
+                utils::get_temp_dir_path(settings::DEFAULT_GENESIS_PATH)
+                    .to_str()
+                    .unwrap(),
+            )
+            .unwrap();
+            let pool_handle = open_test_pool().await;
+
+            let profile: Arc<dyn Profile> =
+                Arc::new(VdrtoolsProfile::new(wallet_handle, pool_handle.clone()));
+
+            async fn indy_teardown(pool_handle: PoolHandle) {
+                delete_test_pool(pool_handle.clone()).await;
+            }
+
+            SetupProfile {
+                institution_did,
+                profile,
+                teardown: Arc::new(move || Box::pin(indy_teardown(pool_handle))),
+            }
+        }
+
+        pub async fn run<F>(f: impl FnOnce(Self) -> F)
+        where
+            F: Future<Output = ()>,
+        {
+            let init = Self::init().await;
+
+            let teardown = Arc::clone(&init.teardown);
+
+            f(init).await;
+
+            (teardown)().await;
+
+            reset_global_state();
+        }
+    }
 
     #[test]
     fn it_works() {}
