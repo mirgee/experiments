dd1b8d523fc90b7308f7a94a524a4b94a1a4fa18
Miroslav Kovar <miroslav.kovar@absa.africa>
2023-04-14 16:23:54 +0200

    did_parser with no regexes
    Signed-off-by: Miroslav Kovar <miroslav.kovar@absa.africa>

--- did_parser/src/lib.rs
+++ did_parser/src/lib.rs
@@ -5,151 +5,157 @@ mod error;
 
 use error::ParseError;
 
+use itertools::Itertools;
 use once_cell::sync::Lazy;
 use regex::Regex;
 use std::{collections::HashMap, ops::Range};
 
 type DIDRange = Range<usize>;
 
-#[derive(Debug, PartialEq, Clone)]
+#[derive(Default)]
 pub struct ParsedDID {
-    did: DIDRange,
     did_url: String,
-    method: DIDRange,
-    id: DIDRange,
-    path: Option<DIDRange>,
-    fragment: Option<DIDRange>,
-    query: Option<DIDRange>,
-    params: HashMap<String, String>, // TODO
+    did: Range<usize>,
+    method: Range<usize>,
+    id: Range<usize>,
+    path: Option<Range<usize>>,
+    query: Option<Range<usize>>,
+    fragment: Option<Range<usize>>,
+    params: HashMap<Range<usize>, Range<usize>>,
 }
 
-trait Default { fn default() -> Self; }
-
-impl Default for ParsedDID {
-    fn default() -> Self {
-        ParsedDID {
-            did: 0..0,
-            did_url: String::new(),
-            method: 0..0,
-            id: 0..0,
-            path: None,
-            fragment: None,
-            query: None,
-            params: HashMap::new(),
-        }
+fn parse_param(rest: &str, start: usize, end: usize) -> Result<(usize, usize, usize), ParseError> {
+    let key_start = start + 1;
+    let value_start = rest[key_start..end]
+        .find('=')
+        .map_or(end, |i| key_start + i + 1);
+    if value_start != end && value_start == key_start {
+        return Err(ParseError::InvalidDIDURL);
     }
+    let next_pos = rest[value_start..end]
+        .find(|c: char| c == ';' || c == '?' || c == '#' || c == '/')
+        .map_or(end, |i| value_start + i);
+
+    Ok((key_start, value_start, next_pos))
 }
-
-pub struct DIDParser;
-
-static DID_MATCHER: Lazy<Regex> = Lazy::new(|| {
-    let pct_encoded = r"(?:%[0-9a-fA-F]{2})";
-    let id_char = &format!(r"(?:[a-zA-Z0-9._-]|{})", pct_encoded);
-    let param_char = r"[a-zA-Z0-9_.:%-]";
-    let param = &format!(r";{}+=({})*", param_char, param_char);
-
-    let method = r"([a-z0-9]+)";
-    let method_id = &format!(r"((?:{}:)*({}+))", id_char, id_char);
-    let params = &format!(r"(({})*)", param);
-    let path = r"(/[^#?]*)?";
-    let query = r"([\?]([^#]*)?)?";
-    let fragment = r"([#](.*)?)?";
-
-    let did_regex_str = &format!(r"^did:{}:{}{}{}{}{}$", method, method_id, params, path, query, fragment);
-
-    Regex::new(did_regex_str).unwrap()
-
-});
 
 impl ParsedDID {
     pub fn parse(did_url: String) -> Result<Self, ParseError> {
-       if did_url.is_empty() {
-            return Err(ParseError::InvalidDIDURL);
+        let method_start = did_url.find(':').ok_or(ParseError::InvalidDIDURL)?;
+        let mut iter = did_url[method_start + 1..].char_indices();
+        let method_end = iter
+            .by_ref()
+            .find(|&(_, c)| c == ':')
+            .map(|(i, _)| i + method_start + 1)
+            .ok_or(ParseError::InvalidDIDURL)?;
+
+        let id_start = method_end + 1;
+        let id_end = iter
+            .by_ref()
+            .find(|&(_, c)| c == ';' || c == '/' || c == '?' || c == '#')
+            .map_or(did_url.len(), |(i, _)| i + method_start + 1);
+
+        let did = 0..id_end;
+        let method = method_start + 1..method_end;
+        let id = id_start..id_end;
+
+        let mut path = None;
+        let mut query = None;
+        let mut fragment = None;
+        let mut params = HashMap::new();
+
+        let mut current_pos = id_end;
+
+        while current_pos < did_url.len() {
+            match did_url.chars().nth(current_pos) {
+                Some(';') => {
+                    let (key_start, value_start, next_pos) =
+                        parse_param(&did_url, current_pos, did_url.len())?;
+                    params.insert(key_start..value_start - 1, value_start..next_pos);
+                    current_pos = next_pos;
+                }
+                Some('/') => {
+                    if path.is_none() {
+                        let path_end = did_url[current_pos..]
+                            .find(|c: char| c == '?' || c == '#' || c == ';')
+                            .map_or(did_url.len(), |i| i + current_pos);
+                        path = Some(current_pos..path_end);
+                        current_pos = path_end;
+                    } else {
+                        current_pos += 1;
+                    }
+                }
+                Some('?') => {
+                    if query.is_none() {
+                        let query_end = did_url[current_pos..]
+                            .find(|c: char| c == '#' || c == ';')
+                            .map_or(did_url.len(), |i| i + current_pos);
+                        query = Some(current_pos + 1..query_end);
+                        current_pos = query_end;
+                    } else {
+                        current_pos += 1;
+                    }
+                }
+                Some('#') => {
+                    if fragment.is_none() {
+                        fragment = Some(current_pos + 1..did_url.len());
+                    }
+                    current_pos += 1;
+                }
+                _ => break,
+            };
         }
 
-       let parsed_did = Self {
-           did_url,
-           ..Default::default()
-       };
-
-       let sections = DID_MATCHER.captures(&parsed_did.did_url).ok_or(ParseError::InvalidDIDURL)?;
-
-       let method = sections.get(1).map(|m| m.range()).unwrap();
-       let id = sections.get(2).map(|m| m.range()).unwrap();
-       let did = 0..sections.get(2).unwrap().end();
-
-       let params = {
-           let mut params = HashMap::new();
-           if let Some(param_str) = sections.get(5).map(|m| m.as_str()) {
-               for param in param_str.split(';')
-                   .filter(|p| !p.is_empty()) {
-                   let kv: Vec<&str> = param.splitn(2, '=').collect();
-                   if kv.len() == 2 {
-                       params.insert(kv[0].to_string(), kv[1].to_string());
-                   } else {
-                       return Err(ParseError::InvalidDIDURL);
-                   }
-               }
-           }
-           params
-       };
-
-       let parsed_did = Self {
-           did,
-           method,
-           id,
-           path: sections.get(7).map(|m| m.range()),
-           query: sections.get(9).map(|m| m.range()),
-           fragment: sections.get(11).map(|m| m.range()),
-           params,
-           ..parsed_did
-       };
-
-       Ok(parsed_did)
+        Ok(ParsedDID {
+            did_url,
+            did,
+            method,
+            id,
+            path,
+            query,
+            fragment,
+            params,
+        })
     }
 
     pub fn did(&self) -> &str {
-        self.did_url[self.did.start..self.did.end].as_ref()
-    }
-
-    pub fn did_url(&self) -> &str {
-        self.did_url.as_ref()
+        &self.did_url[self.did.clone()]
     }
 
     pub fn method(&self) -> &str {
-        self.did_url[self.method.start..self.method.end].as_ref()
+        &self.did_url[self.method.clone()]
     }
 
     pub fn id(&self) -> &str {
-        self.did_url[self.id.start..self.id.end].as_ref()
+        &self.did_url[self.id.clone()]
     }
 
     pub fn path(&self) -> Option<&str> {
-        if let Some(path) = &self.path {
-            Some(self.did_url[path.start..path.end].as_ref())
-        } else {
-            None
-        }
-    }
-
-    pub fn fragment(&self) -> Option<&str> {
-        if let Some(fragment) = &self.fragment {
-            Some(self.did_url[fragment.start..fragment.end].as_ref())
-        } else {
-            None
-        }
+        self.path.as_ref().map(|path| &self.did_url[path.clone()])
     }
 
     pub fn query(&self) -> Option<&str> {
-        if let Some(query) = &self.query {
-            Some(self.did_url[query.start..query.end].as_ref())
-        } else {
-            None
-        }
+        self.query
+            .as_ref()
+            .map(|query| &self.did_url[query.clone()])
     }
 
-    pub fn params(&self) -> &HashMap<String, String> {
-        &self.params
+    pub fn fragment(&self) -> Option<&str> {
+        self.fragment
+            .as_ref()
+            .map(|fragment| &self.did_url[fragment.clone()])
+    }
+
+    pub fn params(&self) -> HashMap<String, String> {
+        self.params
+            .iter()
+            .map(|(k, v)| {
+                (
+                    self.did_url[k.clone()].to_string(),
+                    self.did_url[v.clone()].to_string(),
+                )
+            })
+            .collect()
     }
 }
 
@@ -170,7 +176,7 @@ mod test {
                     assert_eq!(parsed_did.path(), $expected_path, "Path");
                     assert_eq!(parsed_did.query(), $expected_query, "Query");
                     assert_eq!(parsed_did.fragment(), $expected_fragment, "Fragment");
-                    assert_eq!(parsed_did.params(), &$expected_params, "Params");
+                    assert_eq!(parsed_did.params(), $expected_params, "Params");
                 }
             )*
         };
@@ -187,13 +193,12 @@ mod test {
         };
     }
 
-
     test_cases_positive! {
         test_case1: "did:example:123456789abcdefghi", "did:example:123456789abcdefghi", "example", "123456789abcdefghi", None, None, None, HashMap::new()
         test_case2: "did:example:123456789abcdefghi/path", "did:example:123456789abcdefghi", "example", "123456789abcdefghi", Some("/path"), None, None, HashMap::new()
         test_case3: "did:example:123456789abcdefghi/path?query=value", "did:example:123456789abcdefghi", "example", "123456789abcdefghi", Some("/path"), Some("query=value"), None, HashMap::new()
         test_case4: "did:example:123456789abcdefghi/path?query=value#fragment", "did:example:123456789abcdefghi", "example", "123456789abcdefghi", Some("/path"), Some("query=value"), Some("fragment"), HashMap::new()
-        test_case5: "did:example:123456789abcdefghi;param=value", "did:example:123456789abcdefghi", "example", "123456789abcdefghi", None, None, None, { let mut params = HashMap::new(); params.insert("param".to_string(), "value".to_string()); params }
+        test_case5: "did:example:123456789abcdefghi;param1=value1;param2=value2", "did:example:123456789abcdefghi", "example", "123456789abcdefghi", None, None, None, { let mut params = HashMap::new(); params.extend(vec![("param1".to_string(), "value1".to_string()),("param2".to_string(), "value2".to_string())]); params }
     }
 
     test_cases_negative! {
